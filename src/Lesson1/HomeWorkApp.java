30067bd89adaa717bef8fd784cf7ca151fb51f9a
AleksandrZimin <uzao-rec@yandex.ru>
2022-04-13 22:20:10 +0300

    Прошла лекция

---
+++ src/Lesson1/HomeWorkApp.java
@@ -0,0 +1,69 @@
+package Lesson1;
+
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void main(String[] args) {
+        checkSumSign();
+    }
+
+    public static void checkSumSign(){
+        int a = 12, b = 2;
+
+        if ((a + b) > 0) {
+            System.out.println("Summ +");
+        }
+        if ((a + b) < 0) {
+            System.out.println("Summ -");
+        }
+    }
+
+    public static void main(String[] args) {
+        printColor();
+    }
+    public static void printColor(){
+        int value = 150;
+
+        if (value <= 0){
+            System.out.println("Red");
+        }
+        else if (value > 0 & value <= 100){
+            System.out.println("Yellow");
+        }
+        else  {
+            System.out.println("Green");
+        }
+    }
+
+    public static void main(String[] args) {
+        compareNumbers();
+    }
+    public static void compareNumbers(){
+        int a = 12, b = 2;
+
+        if (a >= b){
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+        compareNumbers();
+
+
+    }
+
+
